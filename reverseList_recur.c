d66fe7ffafa726a014bc2e352128136b44147816
tgpb333 <prateek.bhatnagar333@gmail.com>
2025-04-20 10:44:01 +0530

    Create reverseList_recur.c

---
+++ reverseList_recur.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+struct Node{
+  int data;
+  struct Node* next;
+};
+
+struct Node* createNode(int ndata){
+  struct Node* nnode = (struct Node*)malloc(sizeof(struct Node));
+  nnode->data = ndata;
+  nnode->next = NULL;
+  return nnode;
+}
+
+void printList(struct Node* head){
+  struct Node* curr = head;
+  while(curr){
+    printf("%d ", curr->data);
+    curr = curr->next;
+  }
+  printf("\n");
+}
+
+struct Node* reverseList(struct Node* head){
+  if(head == NULL || head->next == NULL)
+    return head;
+  struct Node* rev = reverseList(head->next);
+  head->next->next = head;
+  head->next = NULL;
+  return rev;
+}
+
+int main() {
+
+    // Create a hard-coded linked list:
+    // 1 -> 2 -> 3 -> 4 -> 5
+    struct Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+    head->next->next->next->next = createNode(5);
+
+    printf("Given Linked list:");
+    printList(head);
+
+    head = reverseList(head);
+
+    printf("\nReversed Linked List:");
+    printList(head);
+
+    return 0;
+}
